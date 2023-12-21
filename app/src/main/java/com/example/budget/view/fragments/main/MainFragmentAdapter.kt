baefd909ce0c8a636a97d0137b1cdd9ed97901f7
Sergey Timofeev <Chester_095@mail.ru>
2023-12-21 16:29:59 +0300

    Скорректировал дату

--- app/src/main/java/com/example/budget/view/fragments/main/MainFragmentAdapter.kt
+++ app/src/main/java/com/example/budget/view/fragments/main/MainFragmentAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.budget.view.fragments.main
 
+import android.icu.text.DateFormat
+import android.icu.text.DecimalFormat
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -8,9 +10,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.budget.databinding.ItemMainFragmentBinding
 import com.example.budget.model.database.entity.BudgetEntryEntity
 
+import java.util.Locale
+
 class MainFragmentAdapter(
     private val values: List<BudgetEntryEntity>
 ) : RecyclerView.Adapter<MainFragmentAdapter.MainViewHolder>() {
+
+    private var decimalFormat = DecimalFormat("#,###,###")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
         return MainViewHolder(
@@ -27,10 +33,16 @@ class MainFragmentAdapter(
     override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
         val item = values[position]
         holder.textAccount.text = item.bankAccountId.toString()
-        holder.textDateAndTime.text = item.date.toString()
-        holder.textAmount.text = item.operationAmount.toString()
+        holder.textDateAndTime.text = formatToRusShortDate.format(item.date)
+        holder.textAmount.text = item.operationAmount.formatToText()
 
     }
+
+    private var formatToRusShortDate: DateFormat =
+        DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT, Locale("ru"))
+
+    private fun Number.formatToText() =
+        decimalFormat.format(this).replace(",", " ") + " р."
 
     inner class MainViewHolder(binding: ItemMainFragmentBinding) :
         RecyclerView.ViewHolder(binding.root) {
@@ -42,3 +54,4 @@ class MainFragmentAdapter(
     }
 
 }
+
